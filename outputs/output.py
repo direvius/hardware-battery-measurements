b03915bba29997d09c850d81b1b42a12df2ca2e6
Timur Torubarov <netort@yandex-team.ru>
2016-09-01 19:13:55 +0300

    refactoring outputs.py

--- outputs/output.py
+++ outputs/output.py
@@ -15,25 +15,24 @@ def input_files_to_df(files, suffix, delimiter):
         if work_df is None:
             work_df = pd.read_csv(work_file, delimiter=delimiter, names="ts curr".split())
             work_df['label'] = '{name}{suffix}'.format(name=filename, suffix=suffix)
-            work_df.dropna()
+            #work_df.dropna()
         else:
             df = pd.read_csv(work_file, delimiter=delimiter, names="ts curr".split())
             df['label'] = '{name}{suffix}'.format(name=filename, suffix=suffix)
-            df.dropna()
+            #df.dropna()
             work_df = work_df.append(df)
 
     #convert unixtimestamp to datetime/s
     work_df['ts'] = pd.to_datetime(work_df['ts'],unit='s')
 
     #drop NaN and drop label column
-    df = work_df.groupby('label')
-    return df
+    #df = work_df.groupby('label')
+    return work_df
 
 def render_barplot(df, path):
     logging.info("Started rendering barplot")
     sns.set(font_scale=1, rc={"figure.figsize": (10, 5)})
     ax = sns.barplot(x=df.label, y=df.curr)
-
     for p in ax.patches:
         height = p.get_height()
         ax.text(p.get_x(), height+2, '%1.2f' % (height) )
@@ -57,9 +56,9 @@ def main():
         logging.debug("Started output generation")
         logging.info("Input files: %s", args.files)
 
-        work_dfs = input_files_to_df(args.files, args.suffix, args.delimiter)
-        for key, df in work_dfs:
-            logging.info('Log: %s. Mean: %s', key, df['curr'].mean())
+        df = input_files_to_df(args.files, args.suffix, args.delimiter)
+        for key, grouped_df in df.groupby('label'):
+            logging.info('File: %s. Mean current: %s', key, grouped_df['curr'].mean())
 
         if args.plot == 'barplot':
             render_barplot(df, args.to_dir)
